c9daff9bc6587c045f2bacb5d879c5780edc2954
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-11-18 19:19:45 -0600

    test

--- main.cpp
+++ main.cpp
@@ -3,26 +3,26 @@
 Game *game = nullptr;
 
 int main(int, char **) {
-  const int FPS = 60;
-  const int frameDelay = 1000 / FPS;
-  uint32_t frameStart;
-  int frameTime;
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+	uint32_t frameStart;
+	int frameTime;
 
-  game = new Game();
-  game->init("Craig Game", 0, 0, 800, 600, false);
+	game = new Game();
+	game->init("Craig Game", 0, 0, 800, 600, false);
 
-  while (game->running()) {
-    frameStart = SDL_GetTicks();
+	while (game->running()) {
+		frameStart = SDL_GetTicks();
 
-    game->handleEvents();
-    game->update();
-    game->render();
+		game->handleEvents();
+		game->update();
+		game->render();
 
-    frameTime = SDL_GetTicks() - frameStart;
-    if (frameDelay > frameTime) {
-      SDL_Delay(frameDelay - frameTime);
-    }
-  };
-  game->clean();
-  delete (game);
+		frameTime = SDL_GetTicks() - frameStart;
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);
+		}
+	};
+	game->clean();
+	delete (game);
 }
