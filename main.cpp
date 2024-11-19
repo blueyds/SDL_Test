2fd76738c6936643701677541d78638eec381900
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-11-18 18:51:18 -0600

    added framerate stuff

--- main.cpp
+++ main.cpp
@@ -2,13 +2,26 @@
 
 Game *game = nullptr;
 
-int main(int, char **) {
+int main(int, char **) 
+{
+  const int FPS = 60;
+  const int frameDelay = 1000 / FPS;
+  UInt32 frameStart;
+  int frameTime;
   game = new Game();
   game->init("Craig Game", 0, 0, 800, 600, false);
+  
   while (game->running()) {
+    frameStart = SDL_GetTicks();
+    
     game->handleEvents();
     game->update();
     game->render();
+    
+    frameTime = SDL_GetTicks() - frameStart;
+    if (frameDelay > frameTime){
+      SDL_Delay(frameDelay - frameTime);
+    }
   };
   game->clean();
   delete (game);
