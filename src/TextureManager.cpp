1c39821aa988bf81e8687102ffcd29fa0c3f98fb
Blueyds <blueds@noemail.com>
2024-11-23 11:45:57 -0600

    restaic renderer errors

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -1,14 +1,16 @@
-#include <iostream>
 #include "TextureManager.hpp"
+#include "game.hpp"
+#include <iostream>
 
 SDL_Texture *TextureManager::LoadTexture(const char *fileName) {
   SDL_Surface *tmpSurface = IMG_Load(fileName);
 
   SDL_Texture *texture;
   texture = SDL_CreateTextureFromSurface(Game::renderer, tmpSurface);
-  if (!texture){
+  if (!texture) {
     // txture was invalid
-    std::cout << "[WARNING] Invalid texture from fileName: " << fileName << std:: endl;
+    std::cout << "[WARNING] Invalid texture from fileName: " << fileName
+              << std::endl;
   };
   SDL_FreeSurface(tmpSurface);
   return texture;
--- src/gameObject.cpp
+++ src/gameObject.cpp
@@ -1,29 +1,29 @@
 #include "gameObject.hpp"
 #include "TextureManager.hpp"
+#include "game.hpp"
 
-GameObject::GameObject(const char* textureSheet,int x, int y){
-    objTexture = TextureManager::LoadTexture(textureSheet, ren);
-    xpos = x;
-    ypos = y;
-    srcRect.h  = 32;
-    srcRect.w = 32;
-    srcRect.x = 0;
-    srcRect.y = 0;
-    
+GameObject::GameObject(const char *textureSheet, int x, int y) {
+  objTexture = TextureManager::LoadTexture(textureSheet);
+  xpos = x;
+  ypos = y;
+  srcRect.h = 32;
+  srcRect.w = 32;
+  srcRect.x = 0;
+  srcRect.y = 0;
 }
 
-GameObject::~GameObject(){};
+GameObject::~GameObject() {};
 
-void GameObject::render(){
-    SDL_RenderCopy(Game::renderer, objTexture, &srcRect, &destRect);
+void GameObject::render() {
+  SDL_RenderCopy(Game::renderer, objTexture, &srcRect, &destRect);
 }
 
-void GameObject::update(){
-    xpos++;
-    ypos++;
-    
-    destRect.x = xpos;
-    destRect.y = ypos;
-    destRect.w = srcRect.w * 2;
-    destRect.h = srcRect.h * 2;
+void GameObject::update() {
+  xpos++;
+  ypos++;
+
+  destRect.x = xpos;
+  destRect.y = ypos;
+  destRect.w = srcRect.w * 2;
+  destRect.h = srcRect.h * 2;
 }
