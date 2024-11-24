62eca86c29a2201e5ae7e158610d5723729025c4
Blueyds <blueds@noemail.com>
2024-11-23 21:43:01 -0600

    error cannot load files

--- src/Map.h
+++ src/Map.h
@@ -1,26 +1,23 @@
-#ifndeg MAP_H_12532
+#ifndef MAP_H_12532
 #define MAP_H_12532
 #include "SDL.h"
 
-class Map{
+class Map {
 
 public:
+  Map();
+  ~Map();
 
-    Map();
-    ~Map();
-    
-    void LoadMap(int arr[20][25]);
-    void DrawMap();
-    
+  void LoadMap(int arr[20][25]);
+  void DrawMap();
+
 private:
-    
-    SDL_Rect src, dest;
-    SDL_Texture* dirt;
-    SDL_Tecture* grass;
-    SDL_Texture* water;
-    
-    int map[20][25];
-    
+  SDL_Rect src, dest;
+  SDL_Texture *dirt;
+  SDL_Texture *grass;
+  SDL_Texture *water;
+
+  int map[20][25];
 };
 
 #endif
