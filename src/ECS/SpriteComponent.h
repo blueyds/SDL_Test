5109644d4ce622437550c869d6284113822caccd
Blueyds <blueds@noemail.com>
2024-11-26 13:16:26 -0600

    eror edits

--- src/ECS/SpriteComponent.h
+++ src/ECS/SpriteComponent.h
@@ -1,39 +1,35 @@
 #ifndef SPRITE_COMPONENT_H_5674
 #define SPRITE_COMPONENT_H_5674
+#include "../TextureManager.hpp"
 #include "Components.h"
 #include <SDL.h>
 
-class SpriteComponent: public Component{
+class SpriteComponent : public Component {
 public:
-	SpriteComponent() = default;
-	SpriteComponent(const char* path){
-		setTex(path);
-	}
-	void setTex(const char* path){
-		texture = TextureManager::LoadTexture(path);
-	}
-	void init() override{
-		position = &entity->getComponent<PositionCompnent>();
-		srcRect.y = srcRecr.x = 0;
-		srcRect.h = 32;
-		srcRect.w = 32;
-		destRect.h = srcRect.h * 2;
-		destRect.w = srcRect.w * 2;
-	}
-	
-	void update() override{
-		destRect.x = position->x();
-		destRect.y = position->y();
-	}
-	
-	void draw() override {
-		TextureManager::Draw(texture, srcRect, destRect);
-	}
+  SpriteComponent() = default;
+  SpriteComponent(const char *path) { setTex(path); }
+  void setTex(const char *path) { texture = TextureManager::LoadTexture(path); }
+  void init() override {
+    position = &entity->getComponent<PositionComponent>();
+    srcRect.y = srcRect.x = 0;
+    srcRect.h = 32;
+    srcRect.w = 32;
+    destRect.h = srcRect.h * 2;
+    destRect.w = srcRect.w * 2;
+  }
+
+  void update() override {
+    destRect.x = position->x();
+    destRect.y = position->y();
+  }
+
+  void draw() override { TextureManager::Draw(texture, srcRect, destRect); }
+
 private:
-	PositionComponent *position;
-	SDL_Texture *texture;
-	SDL_Rect srcRect;
-	SDL_Rect destRect;
+  PositionComponent *position;
+  SDL_Texture *texture;
+  SDL_Rect srcRect;
+  SDL_Rect destRect;
 };
 
 #endif
--- src/game.cpp
+++ src/game.cpp
@@ -5,14 +5,13 @@
 #include "game.hpp"
 #include "gameObject.hpp"
 
-#include "Components.h"
-#include "ECS.h"
+#include "ECS/Components.h"
 
-//GameObject *player;
+// GameObject *player;
 Map *map;
 
 Manager manager;
-auto &layer(manager.addEntity());
+auto &player(manager.addEntity());
 
 Game::Game(/* args */) {}
 
@@ -43,7 +42,7 @@ void Game::init(const char *title, int xpos, int ypos, int width, int height,
   } else {
     isRunning = false;
   }
- // player = new GameObject("Sara_16x18_Preview.png", 0, 0);
+  // player = new GameObject("Sara_16x18_Preview.png", 0, 0);
   map = new Map();
   player.addComponent<PositionComponent>();
   player.addComponent<SpriteComponent>("Sara_16x18_Preview.png");
@@ -63,8 +62,8 @@ void Game::handleEvents() {
 }
 
 void Game::update() {
-  //player->update();
-	manager.refresh();
+  // player->update();
+  manager.refresh();
   manager.update();
   int x = player.getComponent<PositionComponent>().x();
   int y = player.getComponent<PositionComponent>().y();
@@ -75,7 +74,7 @@ void Game::render() {
   SDL_RenderClear(renderer);
   // this is where we add stuff to render
   map->DrawMap();
-  //player->render();
+  // player->render();
   manager.draw();
   SDL_RenderPresent(renderer);
 }
